671ccf1d06fa757a8b4f6010168360e750557f9f
James Davis <dvsxutact@users.noreply.github.com>
2019-03-20 15:02:04 -0500

    Update mysql_secure.sh

--- mysql_secure.sh
+++ mysql_secure.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sudo -E mysql -u root <<-EOF
-UPDATE mysql.user SET Password=PASSWORD('$rootpass') WHERE User='root';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-DELETE FROM mysql.user WHERE User='';
-DELETE FROM mysql.db WHERE Db='test' OR Db='test_%';
-FLUSH PRIVILEGES;
-EOF
+$DATABASE_PASS='vagrant'
+
+sudo -E mysqladmin -u root password "$DATABASE_PASS"
+sudo -E mysql -u root -p"$DATABASE_PASS" -e "UPDATE mysql.user SET Password=PASSWORD('$DATABASE_PASS') WHERE User='root'"
+sudo -E mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1')"
+sudo -E mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.user WHERE User=''"
+sudo -E mysql -u root -p"$DATABASE_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\_%'"
+sudo -E mysql -u root -p"$DATABASE_PASS" -e "FLUSH PRIVILEGES"
