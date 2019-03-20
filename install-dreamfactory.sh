cca4034fa26e3fc5bd49bb21aa3bc4d887f8a7ba
James Davis <dvsxutact@users.noreply.github.com>
2019-03-20 16:06:03 -0500

    Update install-dreamfactory.sh

--- install-dreamfactory.sh
+++ install-dreamfactory.sh
@@ -1,6 +1,6 @@
  sudo mkdir /opt/dreamfactory
  
- sudo chown -R dfuser /opt/dreamfactory
+ sudo chown -R vagrant /opt/dreamfactory
  
  cd /opt/dreamfactory
  
@@ -8,7 +8,15 @@
  
  composer install --no-dev
  
- php artisan df:env
+ php artisan df:env --db_connection=mysql --db_host=localhost --db_port=3306 --db_database=dreamfactory --db_username=dfadmin --db_password=dr3amFact0ry!
  
+ echo "DB_CHARSET=utf8" >> /opt/dreamfactory/.env
+ echo "DB_COLLATION=utf8_unicode_ci" >> /opt/dreamfactory/.env
  
+ php artisan df:setup --admin_first_name=vagrant --admin_last_name=vagrant --admin_email=noemail@noeamil.com --admin_password=vagrant
  
+ sudo -E sudo chown -R www-data:vagrant storage/ bootstrap/cache/
+ 
+ sudo -E sudo chmod -R 2775 storage/ bootstrap/cache/
+ 
+ php artisan cache:clear
