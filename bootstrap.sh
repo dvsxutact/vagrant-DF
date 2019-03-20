e55f3f4e8c10eb24f700e91b368b499e1ebd3401
James Davis <dvsxutact@users.noreply.github.com>
2019-03-20 13:41:10 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,7 @@
 # Bootstrap file.
+
+# Always a good idea to apt update after starting a box.
+sudo -E apt update
+
+# now we gotta figure out how to install dreamfactory. lets start by covering depends.
+sudo -E apt-get -q -y install curl apache2 php5 php5-common php5-cli php5-curl php5-mcrypt php5-gd php5-mysql mysql-server mysql-client git
