c63f64e24bd1be6aca007da5d1d60c9cff23dcf1
James Davis <dvsxutact@users.noreply.github.com>
2019-03-20 16:22:33 -0500

    Update install-nginx.sh

--- install-nginx.sh
+++ install-nginx.sh
@@ -1,6 +1,18 @@
 # Install nginx
 sudo -E apt install nginx
 
+## move the default config file to a backup.
+sudo -E mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+
+## Copy the config file into place
+sudo -E cp /vagrant/nginx.conf /etc/nginx/sites-available/default
+
+# Allow Nginx Http through the firewall
+sudo -E /usr/sbin/ufw allow 'Nginx HTTP'
+
+# Allow Nginx Https through the firewall
+sudo -E /usr/sbin/ufw allow 'Nginx HTTPS'
+
 # Restart nginx
 sudo -E service php7.1-fpm restart
 sudo -E service nginx restart
